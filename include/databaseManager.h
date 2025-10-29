bfa29a8139777e6e95df66c38332431263227b12
Nathan <nathansantos1713@gmail.com>
2025-10-29 15:57:23 -0300

    feat: adding databaseManager class to manage sqlite conection and migrations

---
+++ include/databaseManager.h
@@ -0,0 +1,21 @@
+#ifndef DATABASE_MANAGER_H
+#define DATABASE_MANAGER_H
+
+#include "sqlite3.h" // Inclui o sqlite para que ele saiba o que é "sqlite3*"
+#include <string>
+
+class DatabaseManager {
+private:
+  sqlite3 *db; // O ponteiro para o objeto do banco de dados
+
+public:
+  DatabaseManager();  // Construtor
+  ~DatabaseManager(); // Destrutor
+
+  // Método para abrir a conexão
+  bool abrirConexao(const std::string &nomeArquivo);
+
+  bool inicializarTabelas(const std::string &schemaSqlPath);
+};
+
+#endif
---
+++ src/databaseManager.cpp
@@ -0,0 +1,67 @@
+// 1. Inclua a "planta" da classe que você está construindo
+#include "databaseManager.h"
+
+// 2. Inclua o que você precisa para a implementação
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+// Implementação do Construtor
+DatabaseManager::DatabaseManager() : db(nullptr) {
+  // Começa com o banco fechado (ponteiro nulo)
+}
+
+// Implementação do Destrutor
+DatabaseManager::~DatabaseManager() {
+  if (db) {
+    sqlite3_close(db);
+    std::cout << "Conexão com o banco de dados fechada." << std::endl;
+  }
+}
+
+// Implementação do método de abrir
+bool DatabaseManager::abrirConexao(const std::string &nomeArquivo) {
+  int rc = sqlite3_open(nomeArquivo.c_str(), &db);
+
+  if (rc != SQLITE_OK) {
+    std::cerr << "Erro ao abrir o banco de dados: " << sqlite3_errmsg(db)
+              << std::endl;
+    return false;
+  } else {
+    std::cout << "Banco de dados aberto com sucesso: " << nomeArquivo
+              << std::endl;
+    return true;
+  }
+}
+
+bool DatabaseManager::inicializarTabelas(const std::string &schemaSqlPath) {
+  if (!db) {
+    std::cerr << "Erro: Banco de dados não está aberto." << std::endl;
+    return false;
+  }
+
+  std::ifstream schemFile(schemaSqlPath) if (!schemaFile.is_open()) {
+    std::cerr << "Erro ao abrir o arquivo schema.sql em: " << schemaSqlPath
+              << std::sql;
+    return false;
+  }
+
+  std::stringstream buffer;
+  buffer << schemaFile.rdbuf();
+  std::string schemaSql = buffer.str();
+  schemaFile.close();
+
+  char *errMsg = nullptr;
+
+  int rc = sqlite3_exec(db, schemaSql.c_str(), nullptr, nullptr, &errMsg);
+
+  if (rc != SQLITE_OK) {
+    std::cerr << "Erro ao executar o schema SQL: " << errMsg << std::endl;
+    sqlite3_free(errMsg);
+    return false;
+  }
+
+  std::cout << "Tabelas do banco de dados inicializadas com sucesso."
+            << std::endl;
+  return true;
+}
